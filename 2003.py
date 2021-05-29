5ec0d859043593ac80d5108c7a58e110d32cfc07
KGC9175 <rlcks273@naver.com>
2021-05-29 21:57:44 +0900

    1st commit

---
+++ 2003.py
@@ -0,0 +1,17 @@
+# 수들의 합2
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+count = 0
+end = 0
+interval_sum = 0
+
+for start in range(n):
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+
+print(count)
